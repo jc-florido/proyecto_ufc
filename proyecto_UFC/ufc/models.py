2b5246b1e540df8e9790511c4ac20f2ac704edff
jose carlos florido cobo <xjflocob149@ieshnosmachado.org>
2025-01-09 11:19:49 +0100

    peleadores modelo creado

--- proyecto_UFC/ufc/models.py
+++ proyecto_UFC/ufc/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django import forms
+from datetime import date
+
+# Create your models here.
+class Peleadores(models.Model):
+    nombre= models.CharField(max_length=100)
+    apellido=models.CharField(max_length=100)
+    edad=models.IntegerField(max_length=2)
+    codigo= models.CharField(max_length=10,unique=True)
+    fecha_inicio=models.DateField()
+    categoria=models.CharField(max_length=100)
+    posicion=models.CharField(max_length=100)
+    record=models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return f"{self.nombre} - {self.apellido} - {self.edad} - {self.codigo} - {self.fecha_inicio} - {self.categoria} - {self.posicion} - {self.record}"
--- proyecto_UFC/ufc/views.py
+++ proyecto_UFC/ufc/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render,redirect
+from .forms import CursoForm,EstudianteForm,InscripcionForm
+from .models import Curso,Estudiante,Inscripcion
 
 # Create your views here.
+def principal(request):
+    return render(request,'ufc/principal.html')
