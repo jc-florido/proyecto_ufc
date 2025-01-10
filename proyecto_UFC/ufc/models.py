423d877f601fef5a658eb2ec3aa4626f47242072
jose carlos florido cobo <xjflocob149@ieshnosmachado.org>
2025-01-10 11:03:48 +0100

    hecho los modelos

--- proyecto_UFC/ufc/models.py
+++ proyecto_UFC/ufc/models.py
@@ -18,8 +18,8 @@ class Usuarios(models.Model):
 class Peleadores(models.Model):
     nombre= models.CharField(max_length=100)
     apellido=models.CharField(max_length=100)
-    edad=models.IntegerField(max_length=2)
-    codigo= models.CharField(max_length=10,unique=True)
+    edad=models.IntegerField()
+    codigo= models.CharField(max_length=100,unique=True)
     fecha_inicio=models.DateField()
     categoria=models.CharField(max_length=100)
     posicion=models.CharField(max_length=100)
@@ -29,19 +29,55 @@ class Peleadores(models.Model):
     def __str__(self):
         return f"{self.nombre} - {self.apellido} - {self.edad} - {self.codigo} - {self.fecha_inicio} - {self.categoria} - {self.posicion} - {self.record}"
     
+    def clean_fecha_inicio(self):
+        cleaned_data=super().clean()
+        fecha_inicio=cleaned_data.get("fecha_inicio")
+        if fecha_inicio > date.today():
+            raise forms.ValidationError("La fecha no puede ser superior a la actual")
+        
+        return cleaned_data
 
 class Pelea(models.Model):
-    codigo=models.CharField(max_length=100)
-    peleador1=models.ManyToManyField(Peleadores,related_name='Peleadores')
-    peleador2=models.ManyToManyField(Peleadores,related_name='Peleadores')
+    codigo=models.CharField(max_length=100,unique=True)
+    peleador1=models.ManyToManyField(Peleadores,related_name='Peleador_1')
+    peleador2=models.ManyToManyField(Peleadores,related_name='Peleador_2')
     fecha=models.DateField()
+    asaltos=models.IntegerField()
     ganador=models.CharField(max_length=100)
     perdedor=models.CharField(max_length=100)
     categoria=models.CharField(max_length=100)
 
     def __str__(self):
-        return f" {self.codigo} -{self.peleador1} - {self.peleador2} - {self.fecha} - {self.ganador} - {self.perdedor} - {self.categoria}"
+        return f" {self.codigo} - {self.peleador1} - {self.peleador2} - {self.fecha} - {self.ganador} - {self.perdedor} - {self.categoria}"
     
+    def clean_fecha(self):
+        cleaned_data=super().clean()
+        fecha=cleaned_data.get("fecha")
+        if fecha < date.today():
+            raise forms.ValidationError("La fecha no puede ser menor a la actual")
+        
+        return cleaned_data
+    
+    def clean(self):
+        cleaned_data=super().clean()
+        ganador=cleaned_data.get("ganador")
+        perdedor=cleaned_data.get("perdedor")
+
+        if ganador == perdedor:
+            raise forms.ValidationError("El ganador no puede ser el mismo que el perdedor")
+        if ganador == "" or perdedor == "":
+            raise forms.ValidationError("Debe seleccionar un ganador y un perdedor")
+        
+        return cleaned_data
+    
+    def clean_asaltos(self):
+        cleaned_data=super().clean()
+        asaltos=cleaned_data.get("asaltos")
+        if asaltos < 1 and asaltos > 5:
+            raise forms.ValidationError("El asalto no puede ser menor a 1 ni mayor a 5")
+        
+        return cleaned_data
+
     def clean(self):
         cleaned_data = super().clean()
         peleador1 = cleaned_data.get("peleador1")
@@ -51,11 +87,105 @@ class Pelea(models.Model):
         
         return cleaned_data
     
+class Categoria(models.Model):
+    nombre=models.CharField(max_length=100,unique=True)
+    peleadores=models.ManyToManyField(Peleadores,related_name='Peleadores_categoria')
+
+    def __str__(self):
+        return f"{self.nombre} - {self.peleadores}"
+    
 class Evento(models.Model):
-    nombre=models.IntegerField(max_length=100,unique=True)
+    nombre=models.IntegerField(unique=True)
     fecha=models.DateField()
     lugar=models.CharField(max_length=100)
     peleas=models.ManyToManyField(Pelea)
 
     def __str__(self):
         return f"{self.nombre} - {self.fecha} - {self.lugar} - {self.peleas}"
+    
+    def clean_fecha(self):
+        cleaned_data=super().clean()
+        fecha=cleaned_data.get("fecha")
+        if fecha < date.today():
+            raise forms.ValidationError("La fecha no puede ser menor a la actual")
+        
+        return cleaned_data
+    
+class Noticias(models.Model):
+    codigo=models.CharField(max_length=100,unique=True)
+    titulo=models.CharField(max_length=100)
+    contenido=models.CharField(max_length=100000)
+    fecha=models.DateField()
+    autor=models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.codigo} - {self.titulo} - {self.contenido} - {self.fecha} - {self.autor}"
+    
+    def clean_fecha(self):
+        cleaned_data=super().clean()
+        fecha=cleaned_data.get("fecha")
+        if fecha < date.today():
+            raise forms.ValidationError("La fecha no puede ser menor a la actual")
+        
+        return cleaned_data
+    
+class Apuesta(models.Model):
+    codigo=models.CharField(max_length=100,unique=True)
+    usuario=models.ManyToManyField(Usuarios,related_name='Usuarios_apuestas')
+    evento=models.ForeignKey(Evento,on_delete=models.CASCADE,related_name='Evento')
+    pelea=models.ForeignKey(Pelea,on_delete=models.CASCADE,related_name='Pelea')
+    cantidad=models.IntegerField()
+    ganador=models.CharField(max_length=100,blank=True)
+    empate=models.BooleanField(default=False,blank=True)
+    KO=models.BooleanField(default=False,blank=True)
+    asalto=models.IntegerField(blank=True)
+    fecha=models.DateField()
+
+    def __str__(self):
+        return f" {self.codigo} - {self.usuario} - {self.evento} - {self.pelea} - {self.cantidad} - {self.ganador} - {self.empate} - {self.KO} - {self.asalto} - {self.fecha}"
+    
+    def clean_fecha(self):
+        cleaned_data=super().clean()
+        fecha=cleaned_data.get("fecha")
+        if fecha < date.today():
+            raise forms.ValidationError("La fecha no puede ser menor a la actual")
+        
+        return cleaned_data
+    
+    def clean_asalto(self):
+        cleaned_data=super().clean()
+        asalto=cleaned_data.get("asalto")
+        if asalto < 1 and asalto > 5:
+            raise forms.ValidationError("El asalto no puede ser menor a 1 ni mayor a 5")
+        
+        return cleaned_data
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        ganador = cleaned_data.get("ganador")
+        empate = cleaned_data.get("empate")
+
+        if ganador == "" and empate == False:
+            raise forms.ValidationError("Debe seleccionar un ganador o empate")
+        if ganador != "" and empate == True:
+            raise forms.ValidationError("No puede seleccionar un ganador y empate al mismo tiempo")
+        
+        return cleaned_data
+    
+class Comentario(models.Model):
+    codigo=models.CharField(max_length=100,unique=True)
+    usuario=models.ManyToManyField(Usuarios,related_name='Usuarios_comentarios')
+    noticia=models.ForeignKey(Noticias,on_delete=models.CASCADE,related_name='Noticias')
+    contenido=models.CharField(max_length=1000)
+    fecha=models.DateField()
+
+    def __str__(self):
+        return f"{self.codigo} - {self.usuario} - {self.noticia} - {self.contenido} - {self.fecha}"
+    
+    def clean_fecha(self):
+        cleaned_data=super().clean()
+        fecha=cleaned_data.get("fecha")
+        if fecha < date.today():
+            raise forms.ValidationError("La fecha no puede ser menor a la actual")
+        
+        return cleaned_data
